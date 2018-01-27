33f0e758ed3eb2f223238787dc57e53499cb5b5b
Glitch (mammoth-harmonica) <none>
2018-01-27 21:25:15 +0000

    🏅☝️ Updated with Glitch

--- server.js
+++ server.js
@@ -3,20 +3,22 @@
 
 // init project
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+var app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
+app.get("/", function (request, response, next) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
+app.get("/dreams", function (request, response, next) {
   response.send(dreams);
 });
 
